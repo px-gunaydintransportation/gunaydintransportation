046a7edec46e488d798fab9b66a30403e81d860a
HalilSahin <xelilim@hotmail.com>
2019-08-05 01:00:43 +0300

    .NET Core MVC Model added NuGet Packages Installed

---
+++ Gunaydin Transporting/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GunaydinTransporting.web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gunaydin_Transporting.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var user = new Users();
+            user.Id = 1;
+            user.Name = "Halil Şahin";
+            user.Password = "hayuha12";
+            user.PhoneNumber = "5534922220";
+            return View(user);
+        }
+    }
+}
